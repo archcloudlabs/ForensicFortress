3ff7a8e0f318368b2cd40b4b938fb81897c2c6e8
Jared Stroud <1250113+jaredestroud@users.noreply.github.com>
2020-02-06 21:54:48 -0500

    Simple reverse script

--- challenge-2/reverse_image.py
+++ challenge-2/reverse_image.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-#fh = open("Block_dev.img", "r+b") 
-fh = open("rev.img", "r+b") 
+fh = open("Block_dev.img", "r+b") 
 fh.seek(0)
 all_data = fh.read()
 
-fh = open("fwd.img", "w+b") 
+fh = open("rev.img", "w+b") 
 fh.write(all_data[::-1])
